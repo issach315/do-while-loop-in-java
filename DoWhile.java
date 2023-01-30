633f0a4376da910ddc2d80b8f807c030672da469
issach315 <117745188+issach315@users.noreply.github.com>
2023-01-30 11:58:04 +0530

    what is do while loop

--- DoWhile.java
+++ DoWhile.java
@@ -5,6 +5,9 @@ public class DoWhile
 
     public static void main(String[] args) 
     {
+        
+        // in this loop type the first executes the code first in which is present in the do block it willbe executed first and then 
+        // check the condition 
          int i=1;
         do 
         {
