f7d33807b1d73a9dbe9e57310b01aeae8710f010
issach315 <issachemani227@gmial.com>
2023-01-30 11:55:14 +0530

    do-while loop in java

---
+++ DoWhile.java
@@ -0,0 +1,18 @@
+package doWhleLoop;
+
+public class DoWhile
+{
+
+    public static void main(String[] args) 
+    {
+         int i=1;
+        do 
+        {
+            System.out.println("india"); 
+            
+            i++;
+        } while (i<=10);
+        
+    }
+
+}
